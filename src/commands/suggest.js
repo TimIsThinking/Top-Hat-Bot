9bb8b8e7014bf623b1f6285db3463fb25b71ffd4
Tim <13Tim37@gmail.com>
2018-04-05 00:03:00 +0100

    Add suggestion, github link and changelog command

---
+++ src/commands/suggest.js
@@ -0,0 +1,13 @@
+const suggest = (msg, args) => {
+    if (args < 1) {
+        msg.reply(`Suggestion cannot be empty, usage: !suggest <SUGGESTION>`)
+        return
+    } else {
+        // This is pretty terrible but will work for now ...
+        const owner = msg.channel.guild.members.find(member => member.id === msg.channel.guild.ownerID)
+        owner.user.send(`Suggestion from ${msg.author.username}: ${args.join(' ')}`)
+        msg.reply(`Thanks for your suggestion ${msg.author.username}!`)
+    }
+}
+
+module.exports = suggest
